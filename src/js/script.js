2a2878b89c0e5347ff98fad043e81f4d594b6c45
KaikNunes <kaikleonardo30@gmail.com>
2023-07-09 00:10:12 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,25 @@ const personagens = document.querySelectorAll('.personagem')
 
 personagens.forEach(personagem => {
     personagem.addEventListener('click', () => {
+
+        const personagemSelecionado = document.querySelector('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+        console.log(personagemSelecionado)
+
         personagem.classList.add('selecionado');
+
+
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        const idPersonagem = personagem.attributes.id.value;
+
+        imagemPersonagemGrande.src = `./src/img/${idPersonagem}-png.webp`
+
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
     })
 })
